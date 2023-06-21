8dd8cef96fe3448ada2b4400524301f927bab571
Lorne Prince <lorneAprince@gmail.com>
2023-06-21 14:14:27 -0400

    Add project files.

---
+++ SimpleLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
